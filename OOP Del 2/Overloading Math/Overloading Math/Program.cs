d6c47fd3f4b5c45e0f2d5e35a6ae8388471ebab8
Kennie <kenniegroth@gmail.com>
2019-11-19 12:18:33 +0100

    Overload Math Færdig

---
+++ OOP Del 2/Overloading Math/Overloading Math/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Overloading_Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OverMath math = new OverMath();
+            Console.WriteLine(math.Plus("2","3"));
+        }
+    }
+}
