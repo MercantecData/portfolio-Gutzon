c885167f6e6b8ba57d1e6f06383f3b92a602af54
Gutzon <57088465+Gutzon@users.noreply.github.com>
2019-12-16 08:57:58 +0100

    Add files via upload

--- DatabasePro Del 2/Database Management/Udfordring.sql
+++ DatabasePro Del 2/Database Management/Udfordring.sql
@@ -5,10 +5,19 @@ CREATE PROCEDURE generate_user_and_database(
     IN kode TEXT
 )
 BEGIN
-    CREATE USER username ; 
-	SET PASSWORD FOR username = PASSWORD('kode');
-    CREATE DATABASE username_db ; 
-	GRANT SELECT ON *.* TO username ;
-GRANT ALL ON username_db.* TO username ;
+	PREPARE stmt1 FROM CONCAT('CREATE USER ', username);
+	EXECUTE stmt1 ;
+	DEALLOCATE PREPARE stmt1 ;
+	PREPARE stmt2 FROM CONCAT('SET PASSWORD FOR ', username, ' = PASSWORD(\'',kode,'\')');
+	EXECUTE stmt2 ;
+	DEALLOCATE PREPARE stmt2 ;
+	PREPARE stmt3 FROM CONCAT('CREATE DATABASE ', username,'_db');
+	EXECUTE stmt3 ;
+	DEALLOCATE PREPARE stmt3 ;
+	PREPARE stmt4 FROM CONCAT('GRANT SELECT ON *.* TO ', username);
+	EXECUTE stmt4 ;
+	DEALLOCATE PREPARE stmt4 ;
+	PREPARE stmt5 FROM CONCAT('GRANT ALL ON ',username,'_db TO ', username);
+	EXECUTE stmt5 ;
+	DEALLOCATE PREPARE stmt5 ;
 END //
-
